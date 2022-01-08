0aa11dbdb36f767b373feb70f09045005d01de07
Adrian Crespo Ames <Adrian_Ames@outlook.com>
2022-01-08 01:06:14 -0500

    Initial project architecture

---
+++ src/main/java/com/nttdata/bootcamp/activeoperationsservice/ActiveOperationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.activeoperationsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveOperationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveOperationsServiceApplication.class, args);
+	}
+
+}
